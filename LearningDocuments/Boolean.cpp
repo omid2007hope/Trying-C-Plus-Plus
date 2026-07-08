61c75266e784ac75383649f1bc93ada14be7a42d
omid2007hope <omidhope2007@gmail.com>
2026-07-08 10:38:40 +0200

    Refactor code structure for improved readability and maintainability

--- LearningDocuments/Boolean.cpp
+++ LearningDocuments/Boolean.cpp
@@ -31,8 +31,30 @@ int main()
 
     cout << boolalpha; // enable printing "true"/"false"
 
+    /*
+
+    If you prefer to print true and false as words instead of 1 and 0,
+    you can use the boolalpha manipulato
+
+    */
+
     cout << isCodingFun << "\n"; // Outputs true
     cout << isFishTasty << "\n"; // Outputs false
+
+    cout << noboolalpha; // reset to 1/0
+
+    // If you want to go back to the default behavior (printing 1 and 0), you can use noboolalpha:
+
+    cout << isCodingFun << "\n"; // Outputs true
+    cout << isFishTasty << "\n"; // Outputs false
+
+    /*
+
+    Note: It is up to you whether you prefer the default 1 and 0,
+    or the words true and false. Both are correct in C++,
+    and you can switch between them using boolalpha and noboolalpha.
+
+    */
 
     return 0;
 };
