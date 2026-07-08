08cbe930194279529826d01bb028858d9a269bce
omid2007hope <omidhope2007@gmail.com>
2026-07-08 10:02:26 +0200

    Add Math_1.cpp for demonstrating max and min functions

---
+++ LearningDocuments/Math_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+// Include the cmath library
+#include <cmath>
+using namespace std;
+
+int x = 5;
+int y = 10;
+int z = 15;
+
+int main()
+{
+
+    cout << max(x, y, z);
+    cout << min(x, y, z);
+
+    return 0;
+};
