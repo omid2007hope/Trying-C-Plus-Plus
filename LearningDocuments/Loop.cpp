47dd635f927af8c9f07e318aebab148c9de1dad3
omid2007hope <omidhope2007@gmail.com>
2026-07-09 08:08:31 +0200

    Refactor Loop.cpp to implement time tracking with seconds, minutes, and hours

--- LearningDocuments/Loop.cpp
+++ LearningDocuments/Loop.cpp
@@ -3,16 +3,39 @@
 #include <cmath>
 using namespace std;
 
-int num;
+int second;
+int minute;
+int hour;
 
 int main()
 {
 
-    while (num < 5)
+    while (second < 60)
     {
-        num += 1;
-        cout << num;
+        second += 1;
+        cout << "second: " << second << "\n\n";
     };
 
+    while (minute < 60)
+    {
+        if (second == 60)
+        {
+            second -= 60;
+        }
+
+        minute += 1;
+        cout << "minute: " << minute << "\n\n";
+    };
+
+    while (hour < 24)
+    {
+        if (minute == 60)
+        {
+            minute -= 60;
+        };
+
+        hour += 1;
+        cout << "hour: " << hour << "\n\n";
+    }
     return 0;
 };
