8bb124664c31f5d5da2a4bbcbcc537e64dc8b9a3
omid2007hope <omidhope2007@gmail.com>
2026-07-09 07:48:37 +0200

    Refactor code structure for improved readability and maintainability

---
+++ LearningDocuments/Loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// Include the cmath library
+#include <cmath>
+using namespace std;
+
+int num;
+
+int main()
+{
+
+    while (num < 5)
+    {
+        num += 1;
+        cout << num;
+    };
+
+    return 0;
+};
