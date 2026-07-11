d639b70cb7d4b88e3d4a2b7da03b128a2a06d228
omid2007hope <omidhope2007@gmail.com>
2026-07-11 05:06:55 +0200

    Remove obsolete executable files and add a comprehensive ReadMe for C++ learning projects

--- LearningDocuments/BreakAndContinue.cpp
+++ LearningDocuments/BreakAndContinue.cpp
@@ -8,12 +8,28 @@ int main()
         if (num == 10 || num == 30 || num == 50 || num == 70 || num == 90)
         {
 
+            /*
+
+           The continue statement breaks one iteration (in the loop), if a specified condition occurs,
+           and continues with the next iteration in the loop.
+
+            */
+
             continue;
         };
         std::cout << "%" << num << std::endl;
         if (num == 100)
         {
             std::cout << "Done" << std::endl;
+
+            /*
+
+            You have already seen the break statement used in an earlier chapter of this tutorial.
+            It was used to "jump out" of a switch statement.
+            The break statement can also be used to jump out of a loop.
+
+            */
+
             break;
         };
     };
